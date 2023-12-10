45c1a9cb4f4f60c9dd8340b956f0d8f89c358fb8
rfabio8770 <ricardofabio@gmail.com>
2023-12-10 13:19:21 -0300

    Clase 12 Reloj Digital con POO

---
+++ clase 12.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from tkinter import ttk
+import time
+
+# clase que representa la aplicación
+class RelojDigital(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Reloj Digital")
+        self.resizable(0,0)
+        self.geometry("250x80")
+
+        self["bg"] ="black"
+
+        # cambiamos color de fondo
+        self.style = ttk.Style(self)
+        self.style.configure(
+            "TLabel",
+            background = "Black",
+            foreground = "red"
+        )
+        self.label = ttk.Label(self, 
+                               text=self.tiempo_string(),
+                               font=("Digital-7", 40))
+        self.label.pack(expand=True)
+        self.label.after(1000, self.update)
+
+    def tiempo_string(self):
+        return time.strftime('%H:%M:%S')
+    
+    def update(self):
+        self.label.configure(text=self.tiempo_string())
+        self.label.after(1000, self.update)
+
+if __name__== "__main__":
+    reloj = RelojDigital()
+    reloj.mainloop()
